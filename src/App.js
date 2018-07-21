537f3ae95421f628e47f52a53c891738e13bf0ea
Felix Chen <chen.felix.xi@gmail.com>
2018-07-21 15:27:56 +0800

    v1.1.2.1 modified POIs

--- src/App.js
+++ src/App.js
@@ -8,8 +8,8 @@ class App extends Component {
   state = {
     place:[
       { title: 'Home', location: { lat: -36.798376, lng: 174.73672 } },
-      { title: 'Northcote Baptist Church', location: { lat: , lng: } },
-      { title: 'Auckland Zoo', location: { lat: 36.864113, , lng: 174.719685 } },
+      { title: 'Northcote Baptist Church', location: { lat: 36.795293, lng: 174.735701 } },
+      { title: 'Auckland Zoo', location: { lat: 36.864113, lng: 174.719685 } },
       { title: 'OP Shop', location: { lat: 36.799787, lng: 174.747666 } },
       { title: 'Auckland City Backpackers', location: { lat: 36.856863, lng: 174.762632} }
     ]
