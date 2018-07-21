08b311682d1f61909db7cd3ef1967f0411e82c05
Felix Chen <chen.felix.xi@gmail.com>
2018-07-21 22:33:16 +0800

    v1.3.1 design Map componet

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './style.css';
+import List from './List';
 import { Container } from './Container';
 // ref :https://www.fullstackreact.com/articles/how-to-write-a-google-maps-react-component/#
 
@@ -9,10 +9,10 @@ class App extends Component {
   state = {
     place:[
       { title: 'Home', location: { lat: -36.798376, lng: 174.73672 } },
-      { title: 'Northcote Baptist Church', location: { lat: 36.795293, lng: 174.735701 } },
-      { title: 'Auckland Zoo', location: { lat: 36.864113, lng: 174.719685 } },
-      { title: 'OP Shop', location: { lat: 36.799787, lng: 174.747666 } },
-      { title: 'Auckland City Backpackers', location: { lat: 36.856863, lng: 174.762632} }
+      { title: 'Northcote Baptist Church', location: { lat: -36.795293, lng: 174.735701 } },
+      { title: 'Auckland Zoo', location: { lat: -36.864113, lng: 174.719685 } },
+      { title: 'OP Shop', location: { lat: -36.799787, lng: 174.747666 } },
+      { title: 'Auckland City Backpackers', location: { lat: -36.856863, lng: 174.762632} }
     ]
   }
   render() {
@@ -30,6 +30,7 @@ class App extends Component {
           />
         </div>
       </div>
+    )
   }
 }
 export default App;
--- src/Map.js
+++ src/Map.js
@@ -3,9 +3,53 @@ import ReactDom from 'react-dom';
 import './style.css';
 
 class Map extends React.Component {
+
+    Map.propTypes = {
+        google: React.PropTypes.object,
+        zoom: React.PropTypes.number,
+        initialCenter: React.PropTypes.object
+    }
+    Map.defaultProps = {
+        zoom: 13,
+        // Auckland, by default
+        initialCenter: {
+            lat: -36.84846,
+            lng: 174.763332
+        }
+    }
+
+    componentDidMount() {
+        this.loadMap();
+    }
+
+    loadMap() {
+        if (this.props && this.props.google) {
+            // google is available
+            const { google } = this.props;
+            const maps = google.maps;
+
+            //grab a reference to the DOM component where we want the map to be placed
+            const mapRef = this.refs.googlemap;
+            //The node variable above is a reference to the actual DOM element on the page
+            const node = ReactDom.findDOMNode(mapRef);
+
+            let zoom = 14;
+            let lat = -36.795293;
+            let lng = 174.735701;
+            const center = new maps.LatLng(lat,lng);
+            const mapConfig = Object.assign({}, {
+                center: center,
+                zoom: zoom
+            })
+            this.map = new maps.Map(node, mapConfig);
+        }
+
+    }
     render() {
         return(
-
+            <div ref='googlemap'>
+                Loading Map...
+            </div>
         )
     }
 }
