e92c7926be8feeb6cebf9f4655cc2cdcaad7390f
Felix Chen <chen.felix.xi@gmail.com>
2018-07-25 13:28:46 +0800

    modified issues

--- src/Container.js
+++ src/Container.js
@@ -44,7 +44,7 @@ export class Container extends React.Component {
             //     // bounds={bounds}
             //     >
             //   <Marker />
-            </Map>
+            // </Map>
         )
     }
 }
